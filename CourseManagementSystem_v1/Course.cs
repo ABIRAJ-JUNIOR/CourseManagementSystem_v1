3e919fc041454204c195ee373ef62c7dc95dd0ed
UT01102 <UT01102@UNICOMTIC5>
2024-10-11 14:17:40 +0530

    Add project files.

---
+++ CourseManagementSystem_v1/Course.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    internal class Course
+    {
+        private string CourseId { get; set; }
+        private string Title { get; set; }
+        private string Duration { get; set; }
+        private decimal Price { get; set; }
+
+        public Course()
+        {
+        }
+
+        public Course(string courseId, string title, string duration, decimal price)
+        {
+            CourseId = courseId;
+            Title = title;
+            Duration = duration;
+            Price = price;
+        }
+
+        public void SetCourseId(string courseId)
+        {
+            this.CourseId = courseId;
+        }
+
+        public void SetTitle(string title) 
+        { 
+            this.Title = title; 
+        }
+
+        public void SetDuration(string duration)
+        {
+            this.Duration = duration;
+        }
+
+        public void SetPrice(decimal price)
+        {
+            this.Price = price; 
+        }
+
+        public string GetCourseId()
+        {
+            var id = this.CourseId;
+            return id;
+        }
+        public string GetTitle()
+        {
+            var title = this.Title;
+            return title;
+        }
+        public string GetDuration()
+        {
+            var duration = this.Duration;
+            return duration;
+        }
+        public decimal GetPrice()
+        {
+            var price = this.Price;
+            return price;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {this.GetCourseId()}, Title: {this.GetTitle()}, Duration: {this.GetDuration()}, Price: {this.GetPrice()}";
+        }
+    }
+}
---
+++ CourseManagementSystem_v1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var course = new Course();
+            //course.SetCourseId("C_001");
+            //course.SetTitle("python for beginers");
+            //course.SetDuration("3 months");
+            //course.SetPrice(10);
+
+            //course.GetCourseId();
+            //course.ToString();
+            //Console.ReadKey();
+
+
+            bool exit = true;
+            while (exit)
+            {
+                Console.Clear();
+                Console.WriteLine("--- Course Management System ---");
+                Console.WriteLine("1. Add a Course");
+                Console.WriteLine("2. View");
+                Console.WriteLine("3. Add a Course");
+                Console.WriteLine("4. Add a Course");
+                Console.WriteLine("5. Add a Course");
+                Console.WriteLine("6. Add a Course");
+            }
+        }
+    }
+}
